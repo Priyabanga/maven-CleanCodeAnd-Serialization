ddbefbdc6b8a3a41dfc631f167f119b28aa47192
Priyabanga <priya.banga_ccv17@gla.ac.in>
2020-02-20 11:16:46 -0800

    Final Commit

---
+++ maven-CleanCode/src/main/java/com/epam/maven_CleanCode/SIandCI.java
@@ -0,0 +1,10 @@
+package com.epam.maven_CleanCode;
+
+public class SIandCI{
+	public static double simpleInterest(double pr,double t,double r){
+		return (pr*t*r)/100;
+}
+	public static double compoundInterest(double pr,double t,double r){
+		return pr * Math.pow(1.0+r/100.0,t) - pr;
+}
+}
